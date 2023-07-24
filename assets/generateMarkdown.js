29d3f677b685d2515726290635c0d8e5304de270
Cristofferb7 <136852733+Cristofferb7@users.noreply.github.com>
2023-07-24 15:57:05 -0400

    added js changes

--- assets/generateMarkdown.js
+++ assets/generateMarkdown.js
@@ -1,7 +1,25 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+if (license === 'Apache 2.0 License'){
+ return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)' 
+}
+else if  (license === 'Boost Software License 1.0'){
+  return '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
+}
 
+else if  (license === 'BSD 3-Clause License'){
+  return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+}
+
+else if  (license === 'ISC License (ISC)'){
+  return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
+}
+
+else if (license === 'none'){
+return ''
+}
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
@@ -12,7 +30,22 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title} 
+  
+  ## Description 
+  ${data.description}
+
+  ## Installion instructions
+  ${data.installion}
+
+  ##Contribution Guidelines
+  ${data.contribution}
+
+  ##Instructions
+  ${data.instructions}
+
+  ##Licenses 
+  ${renderLicenseBadge(data.license)} 
 
 `;
 }
--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,69 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// Define your list of questions
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "Title",
+     
+  },
+
+  {
+    type: "input",
+    name: "description",
+    message: "Insert Description",
+     
+  },
+
+  {
+    type: "input",
+    name: "installation",
+    message: "Describe instruction",
+  },
+
+  {
+    type: "input",
+    name: "contribution",
+    message: "guidelines",
+  },
+
+  {
+    type: "input",
+    name: "instructions",
+    message: "insert instructions",
+  },
+
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license",
+    choices: ['Apache 2.0 License', 'Boost Software License 1.0', 'BSD 3-Clause License', 'ISC License (ISC)', 'none']
+    //
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((answers) => {
+      console.log("Survey results:");
+      console.log(answers);
+    })
+    .catch((error) => {
+      console.error("Error occurred:", error);
+    });
+
+}
 
 // Function call to initialize app
-init();
+
+
+init()
+
